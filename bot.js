753d71dba2e4332a196a7a18625e282d86c7baf7
DarkDante13 <42676717+DarkDante13@users.noreply.github.com>
2018-08-25 16:29:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NDgyMjc2MDY4NTk5MDA1MjA0.DmFp5A.4uCFbVRnlv3ac);
